413276f2ff37f6c521c2e5912219db3c84dc80ea
Jossany Moura <mjossany@gmail.com>
2022-01-24 17:15:46 -0300

    Cria tabela e model de products

---
+++ back-end/src/database/migrations/20220124200531-create-products.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      url_image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
