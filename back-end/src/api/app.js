0bbd4f64a617aaf86b7ce1c2e0a629b5216ce737
Jossany Moura <mjossany@gmail.com>
2022-01-25 15:30:06 -0300

    Cria root e router para a rota de login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { json } = require('body-parser');
+const root = require('../database/controllers/root');
 
 const app = express();
+
+app.use(json());
+
+app.use('/', root);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
-
-//
