ae173fe4298e3a66469e5165224a39461a2221c6
netosilv4 <netosilv44445@gmail.com>
2022-01-27 00:49:43 -0300

    Fix eslint

--- front-end/src/pages/CustomerProducts.js
+++ front-end/src/pages/CustomerProducts.js
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-function CustomerProducts() {
-  return (
-    <div>
-      // aqui vai de novo o componente da barra de cima de navegacao 
-      // aqui vai o componente do botao de finalizar com o testId="customer_products__button-cart" 
-    </div>
-  );
-}
+// function CustomerProducts() {
+//   return (
+//     <div>
+//       // aqui vai de novo o componente da barra de cima de navegacao
+//       // aqui vai o componente do botao de finalizar com o testId="customer_products__button-cart"
+//     </div>
+//   );
+// }
 
-export default CustomerProducts;
+// export default CustomerProducts;
