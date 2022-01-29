76821efd3bddc5696a2bccb3e67cf83db0a24c02
netosilv4 <netosilv44445@gmail.com>
2022-01-29 18:08:31 -0300

    fix proptypes

--- front-end/src/pages/customerProducts/components/productOverview.js
+++ front-end/src/pages/customerProducts/components/productOverview.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
 const ProductOverview = ({ product }) => {
-  const { name, price, url_image } = product;
+  const { name, price } = product;
   return (
     <div>
-      <img src={ url_image } alt={ name } />
       <h1>{name }</h1>
       <p>{price}</p>
     </div>
   );
+};
+
+ProductOverview.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default ProductOverview;
