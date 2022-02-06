ec9dda6b487893cb757117400b08a42254725607
Jossany Moura <mjossany@gmail.com>
2022-02-06 20:17:55 -0300

    Finaliza a rota para pegar todas as ordens de um determinado cliente no backend

--- back-end/src/database/controllers/customer/router.js
+++ back-end/src/database/controllers/customer/router.js
@@ -4,12 +4,14 @@ const createSale = require('./checkout/createSale');
 const tokenValidation = require('../../middlewares/tokenValidation');
 const getOrderDetails = require('./checkout/getOrderDetails');
 const getAllSellers = require('./checkout/getAllSellers');
+const getAllCustomerOrders = require('./orders/getAllCustomerOrders');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/products', getAllProducts);
 router.post('/checkout', tokenValidation, createSale);
 router.get('/checkout/sellers', getAllSellers);
+router.get('/orders', getAllCustomerOrders);
 router.get('/orders/:idVenda', getOrderDetails);
 
 module.exports = router;
