512191e74188a4ab35d7a2e8db54c2681cb7f451
robbastos <roberto.bastos@prolins.com.br>
2022-02-10 08:25:58 -0300

    corrige identação

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      {
+        {
           !user && (
             <>
               <Route exact path="/login" element={ <Login /> } />
